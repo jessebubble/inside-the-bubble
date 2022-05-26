710c7133a0c4411e295ab8cc8eae135129890b98
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-05-26 17:57:35 -0500

    react-hot-toast setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css' */
 import Link from 'next/link';
+import toast from 'react-hot-toast';
 
 import Loader from '../components/Loader';
 
@@ -16,6 +17,10 @@ export default function Home() {
       </Link>
 
       <Loader show />
+
+      <button onClick={() => toast.success('hello world!')}>
+        Toast Me
+      </button>
     </div>
   )
 }
