ebdfe8f2b8c3299dc3de0e5056b3e43bf1e28a45
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-05-26 16:27:47 -0500

    page routing though nextJS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
-import Head from 'next/head'
+/* import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css' */
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
+      <Link prefetch={false} href={{
+        pathname: '/[username]',
+        query: { username: 'jessebubble' },
+      }}>
+        <a>Inside The Bubble</a>
+      </Link>
     </div>
   )
 }
