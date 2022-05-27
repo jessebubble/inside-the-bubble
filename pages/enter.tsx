6bd3a4d876889ca286374cf468a552caf888a466
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-05-26 21:35:30 -0500

    google auth signin, readme updated

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,39 @@
-export default function EnterPage({}) {
+import { auth, firestore, googleAuthProvider } from '../lib/firebase';
+
+/* eslint-disable @next/next/no-img-element */
+
+export default function Enter(props) {
+    const user = null;
+    const username = null;
+    // 1. user signed out SHOW <SignInButton />
+    // 2. user signed in, but missing username SHOW <UsernameForm />
+    // 3. user signed in, has username SHOW <SignOutButton />
     return (
         <main>
-            <h1>Sign Up</h1>
+            {user ?
+                !username ? <UsernameForm /> : <SignOutButton /> 
+                : 
+                <SignInButton /> 
+            }
         </main>
-    )
+    );
+}
+// Sign in with google button
+function SignInButton() {
+    const signInWithGoogle = async () => {
+        await auth.signInWithPopup(googleAuthProvider);
+    };
+    return (
+        <button className='btn-google' onClick={signInWithGoogle}>
+            <img src={'/google.png'} /> Sign in with Google
+        </button>
+    );
+}
+// Sign out button
+function SignOutButton () {
+    return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+// username form
+function UsernameForm() {
+    return null;
 }
