c1f34cd1747995fab0f0e85732be5b040ea8c971
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-05-31 18:08:04 -0500

    server side home page blog feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,73 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css' */
 import Link from 'next/link';
 import toast from 'react-hot-toast';
-
 import Loader from '../components/Loader';
 
-export default function Home() {
+import PostFeed from '../components/PostFeed';
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
+
+import { useState } from 'react';
+
+// Max post to query per page
+const LIMIT = 10;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      <Link prefetch={false} href={{
-        pathname: '/[username]',
-        query: { username: 'jessebubble' },
-      }}>
-        <a>Inside The Bubble</a>
-      </Link>
-
-      <Loader show />
-
+    <main>
+      <div>
       <button onClick={() => toast.success('hello world!')}>
         Toast Me
       </button>
-    </div>
-  )
+      </div>
+
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end!'}
+    </main>
+  );
 }
