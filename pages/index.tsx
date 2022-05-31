6526111f1d3f33e17e2849e678ce9318d0c77e61
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-05-31 18:56:20 -0500

    post content setup, react markdown installed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 /* import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css' */
-import Link from 'next/link';
+import styles from '../styles/Home.module.css' 
+import Link from 'next/link'; */
 import toast from 'react-hot-toast';
 import Loader from '../components/Loader';
 
