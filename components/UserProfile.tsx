471d9af8df30f7c99e2f0e8ca31c4942a81dfb4c
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-05-31 17:35:01 -0500

    server side rendering of userProfile

---
+++ components/UserProfile.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+
+export default function UserProfile ({ user }) {
+    return (
+        <div className="box-center">
+            <img src={user.photoURL} className="card-img-center" />
+            <p>
+                <i>@{user.username}</i>
+            </p>
+             <h1>{user.displayName}</h1>
+        </div>
+        
+    );
+}
